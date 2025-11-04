69c24676fa733b5370f438aeabe1cefb6589aa28
Lars <larsvangisbergen@gmail.com>
2025-11-04 17:19:37 +0100

    Initial clean commit

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+# Global settings and constants for the simulation
+
+# Screen dimensions
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+FPS = 60  # Frames per second
+
+# Colors
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_BG = (25, 25, 35) # A dark blue/grey background
